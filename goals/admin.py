272b0d902bf209cdf4ed4f214156824aa61602e7
MIETZ <MICHAEL.MIETZ@disney.com>
2020-11-20 12:36:32 +0100

    created Goals and Like models

--- goals/admin.py
+++ goals/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Profile, Goal, Like
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(Goal)
+admin.site.register(Like)
--- goals/models.py
+++ goals/models.py
@@ -8,11 +8,52 @@ class User(AbstractUser):
 
 class Profile(models.Model):
     user                =   models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture     =   models.URLField()
+    profile_picture     =   models.URLField(blank=True)
     date_of_birth       =   models.DateField(blank=True)
 
 class Goal(models.Model):
-    pass
+    term_choices = [
+        ("FT", "Fixed Term"), 
+        ("LT", "Life-time") 
+        ]
+    category_choices = [
+        ("FIT", "Health / Fitness"),
+        ("PRO", "Career"),
+        ("FIN", "Financial"),
+        ("PERS", "Personal"),
+        ("SPIR", "Spiritual"),
+        ("FAM", "Family")
+    ]
+    status_choices = [
+        ("NS", "Not started"),
+        ("IP", "In Progress"),
+        ("AC", "Accomplished")
+    ]
+    title               =   models.CharField(max_length=100)
+    created_by          =   models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at          =   models.DateField(auto_now=True)
+    updated_at          =   models.DateField(auto_now=True)
+    term                =   models.CharField(max_length=50, choices=term_choices)
+    deadline            =   models.DateField()
+    category            =   models.CharField(max_length=50, choices=category_choices)
+    description         =   models.TextField(max_length=140, blank=True)
+    purpose             =   models.TextField(max_length=140, blank=True)
+    status              =   models.CharField(max_length=50, choices=status_choices, default=status_choices[1][1])
+    private             =   models.BooleanField(default=False)
+    likes               =   models.ManyToManyField(User, blank=True, related_name="users_like")
+    support             =   models.ManyToManyField(User, blank=True,  related_name="users_support")
+    copied_by           =   models.ManyToManyField(User, blank=True,  related_name="users_copies")
+    copied_times        =   models.IntegerField(default=0)
+    # origin              =   models.ForeignKey("self")
 
-class NewClass(models.Model):
-    pass
+    def __str__(self):
+        return f"{self.created_by} - {self.title}"
+
+    @property
+    def num_likes(self):
+        return self.likes.all().count()
+
+class Like(models.Model):
+    liked_by            =   models.ForeignKey(User, on_delete=models.CASCADE)
+    liked_goal          =   models.ForeignKey(Goal, on_delete=models.CASCADE)
+    liked_at            =   models.DateField(auto_now=True)
